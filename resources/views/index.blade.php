3e5ae4b981466fb210a67235c0ec49e4cd006e04
irfan <irfan.pragma@gmail.com>
2024-05-13 20:11:19 +0700

    add : bootstrap

---
+++ resources/views/index.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <a href="">Administrator Menu</a>
+        <a href="{{route('users')}}">User Client Menu</a>
+        <a href="">User Teknisi Menu</a>
+    </ul>
+
+    <ul>
+        <li id="id1">q</li>
+        <li id="id2"></li>
+        <li id="id3"></li>
+    </ul>
+</body>
+</html>
